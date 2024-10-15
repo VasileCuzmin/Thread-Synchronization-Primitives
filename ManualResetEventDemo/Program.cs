265ea50760aab9f3330b955d71b2a447f161ed70
vasilecuzmin <vasile.cuzmin@simpology.com>
2024-10-15 14:34:40 +0300

    second

---
+++ ManualResetEventDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using ManualResetEventSlim manualResetEventSlim = new ManualResetEventSlim(false);
+
+Console.WriteLine("Press enter to release all threads...");
+
+//start the worker thread 
+for (int i = 0; i < 3; i++)
+{
+    Thread workerThread = new Thread(Work);
+    workerThread.Name = $"Worker {i + 1}";
+    workerThread.Start();
+}
+
+Console.ReadLine();
+
+manualResetEventSlim.Set();
+
+Console.ReadLine();
+
+
+void Work()
+{
+    Console.WriteLine($"{Thread.CurrentThread.Name} is waiting for the signal");
+
+    manualResetEventSlim.Wait();
+
+    Thread.Sleep(3000);// Simulates working time
+
+    Console.WriteLine($"{Thread.CurrentThread.Name} has been released");
+}
---
+++ ProducerConsumer-TwoWaySignaling/Program.cs
@@ -0,0 +1,74 @@
+﻿//https://www.youtube.com/watch?v=ynwzwBjkWEA&list=PLgRlicSxjeMMYTN01qiQ10aLKEWeSEs-v&index=5
+
+using System.Collections.Concurrent;
+
+ConcurrentQueue<int> queue = new();
+var consumeEvent = new ManualResetEventSlim(false);
+var produceEvent = new ManualResetEventSlim(true);
+
+int consumerCount = 0;
+object lockConsumerCount = new object();
+
+Thread[] consumerThreads = new Thread[3];
+for (int i = 0; i < 3; i++)
+{
+    consumerThreads[i] = new Thread(Consume)
+    {
+        Name = $"Consumer {i+1}"
+    };
+    consumerThreads[i].Start();
+}
+
+//Producer
+while (true)
+{
+    produceEvent.Wait();
+    produceEvent.Reset();//signalled=false - we dont want to produce again until the queue is empty
+
+    Console.WriteLine("To produce, enter 'p'");
+    var input = Console.ReadLine() ?? "";
+
+    if (input.ToLower() == "p")
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            queue.Enqueue(i);
+            Console.WriteLine($"Produced {i}");
+        }
+
+        consumeEvent.Set();//signalled=true
+    }
+}
+
+//Consumer's behavior
+void Consume()
+{
+    while (true)
+    {
+        consumeEvent.Wait();
+
+        while (queue.TryDequeue(out int item))
+        {
+            //work on the items produced
+
+            Thread.Sleep(1500);
+
+            Console.WriteLine($"Cosumed: {item} from thread {Thread.CurrentThread.Name}");
+        }
+
+        lock (lockConsumerCount)
+        {
+            consumerCount++;
+            if (consumerCount == 3) //we have 3 consumer threads
+            {
+                consumeEvent.Reset(); //turn off the signal for the consumers so that the producer could have enough time to produce
+                produceEvent.Set(); //produce again if the queue is empty
+                consumerCount = 0;
+
+                Console.WriteLine("*******************");
+                Console.WriteLine("More please!");
+                Console.WriteLine("*******************");
+            }
+        }
+    }
+}
