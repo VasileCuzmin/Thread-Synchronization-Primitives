e1a99c16eb9281134ccf00c92352ba0c6bd8f23a
vasilecuzmin <vasile.cuzmin@simpology.com>
2024-10-17 09:31:02 +0300

    thread sync

---
+++ ThreadSyncronization/Program.cs
@@ -0,0 +1,33 @@
+﻿int counter = 0;//shared resource between multiple threads
+object objLock = new();
+
+Thread thread1 = new Thread(IncrementCounter);
+thread1.Start();
+
+Thread thread2 = new Thread(IncrementCounter);
+thread2.Start();
+
+
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine($"Final counter value is {counter}");
+
+
+void IncrementCounter()
+{
+    //for (int i = 0; i < 100000; i++)
+    //{
+    //    counter += 1;//non-deterministic result each time - critical section - race condition
+    //}
+
+    for (int i = 0; i < 100000; i++)
+    {
+        lock (objLock)
+        {
+            counter += 1;
+        }
+
+        //Interlocked.Increment(ref counter); // the same as the below
+    }
+}
